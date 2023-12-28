786fcc8da40cc709711da8b6de25accf55612f1f
Nery José Barrientos Posadas <68959243+NeryBarrientos@users.noreply.github.com>
2023-12-27 23:53:34 -0600

    Editar Facturas

--- djangoProject/app/Factura/views.py
+++ djangoProject/app/Factura/views.py
@@ -18,6 +18,7 @@ def facturas(request):
     html = objetoTemplate.render()
     return HttpResponse(html)
 
+
 @csrf_exempt
 def facturas_crear(request):
     template_name = "facturas/menu_facturas_crear.html"
@@ -85,23 +86,24 @@ def procesar_pedido(request):
         ET.SubElement(nueva_factura, 'fecha').text = fecha
 
         # Crear elementos de productos
-    total_factura = 0
-    for producto_data in array_productos:
-        nuevo_producto = ET.Element('producto', id=producto_data[0])
-        ET.SubElement(nuevo_producto, 'nombre').text = producto_data[1]
-        ET.SubElement(nuevo_producto, 'descripcion').text = producto_data[2]
-        ET.SubElement(nuevo_producto, 'cantidad').text = producto_data[3]
-        ET.SubElement(nuevo_producto, 'stock').text = producto_data[4]
-        ET.SubElement(nuevo_producto, 'preciounitario').text = producto_data[5]
-        ET.SubElement(nuevo_producto, 'total').text = producto_data[6]
-        nueva_factura.append(nuevo_producto)
+        total_factura = 0  # Variable para almacenar el total de la factura
+        for producto_data in array_productos:
+            nuevo_producto = ET.Element('producto', id=producto_data[0])
+            ET.SubElement(nuevo_producto, 'nombre').text = producto_data[1]
+            ET.SubElement(nuevo_producto,
+                          'descripcion').text = producto_data[2]
+            ET.SubElement(nuevo_producto, 'cantidad').text = producto_data[3]
+            ET.SubElement(nuevo_producto, 'stock').text = producto_data[4]
+            ET.SubElement(nuevo_producto,
+                          'preciounitario').text = producto_data[5]
+            ET.SubElement(nuevo_producto, 'total').text = producto_data[6]
+            nueva_factura.append(nuevo_producto)
 
-        # Sumar el total del producto al total de la factura
-        total_factura += float(producto_data[6])
+            # Sumar al total de la factura
+            total_factura += float(producto_data[6])
 
-        # Crear el elemento totalfactura y asignarle el total calculado
-        ET.SubElement(nueva_factura, 'totalfactura').text = "{:.2f}".format(
-            total_factura)
+        # Agregar la etiqueta totalfactura al final
+        ET.SubElement(nueva_factura, 'totalfactura').text = str(total_factura)
 
         root.append(nueva_factura)
 
@@ -148,7 +150,8 @@ def eliminar_factura(request):
             root = ET.Element('Facturas')
             tree = ET.ElementTree(root)
 
-        elemento_a_eliminar = root.find(f'.//factura[@correlativo="{correlativo}"]')
+        elemento_a_eliminar = root.find(
+            f'.//factura[@correlativo="{correlativo}"]')
         print(elemento_a_eliminar)
         if elemento_a_eliminar is not None:
             root.remove(elemento_a_eliminar)
@@ -167,6 +170,7 @@ def eliminar_factura(request):
 
     return render(request, template_name, context)
 
+
 @csrf_exempt
 def listar_facturas(request):
     # Cargar la plantilla
@@ -182,7 +186,7 @@ def listar_facturas(request):
         # Cargar datos de facturas
         tree_facturas = ET.parse(archivo_facturas_path)
         root_facturas = tree_facturas.getroot()
-        
+
         for factura in root_facturas.findall("factura"):
             # Extraer datos relevantes de la factura
             numero = factura.get("correlativo")
@@ -221,7 +225,8 @@ def listar_facturas(request):
                 "cliente_nombre": cliente_nombre,
                 "cliente_direccion": cliente_direccion,
                 "total_factura": total_factura,
-                "lst_productos": lst_productos  # Agregar la lista de productos a los datos de la factura
+                # Agregar la lista de productos a los datos de la factura
+                "lst_productos": lst_productos
             }
             lst_facturas.append(factura_data)
 
@@ -232,3 +237,245 @@ def listar_facturas(request):
     # Renderizar la plantilla con los datos organizados
     context = {'lst_facturas': lst_facturas}
     return render(request, "facturas/menu_facturas_listar.html", context)
+
+
+@csrf_exempt
+def facturas_editar(request):
+    # Cargar la plantilla
+    template = loader.get_template("facturas/menu_facturas_editar.html")
+
+    # Listas para almacenar datos de facturas y productos
+    lst_facturas = []
+
+    archivo_facturas_path = os.path.join(
+        settings.BASE_DIR, 'app', 'Factura', 'datos', 'facturas.xml')
+
+    try:
+        # Cargar datos de facturas
+        tree_facturas = ET.parse(archivo_facturas_path)
+        root_facturas = tree_facturas.getroot()
+
+        for factura in root_facturas.findall("factura"):
+            # Extraer datos relevantes de la factura
+            numero = factura.get("correlativo")
+            fecha = factura.find("fecha").text
+            cliente_nit = factura.find("cliente").get("nit")
+            cliente_nombre = factura.find("cliente/nombre").text
+            cliente_direccion = factura.find("cliente/direccion").text
+            total_factura = factura.find("totalfactura").text
+
+            # Extraer datos de productos dentro de la factura
+            lst_productos = []
+            for producto in factura.findall("producto"):
+                producto_id = producto.get("id")
+                producto_nombre = producto.find("nombre").text
+                producto_descripcion = producto.find("descripcion").text
+                producto_cantidad = producto.find("cantidad").text
+                producto_stock = producto.find("stock").text
+                producto_precio_unitario = producto.find("preciounitario").text
+                producto_total = producto.find("total").text
+                producto_data = {
+                    "id": producto_id,
+                    "nombre": producto_nombre,
+                    "descripcion": producto_descripcion,
+                    "cantidad": producto_cantidad,
+                    "stock": producto_stock,
+                    "precio_unitario": producto_precio_unitario,
+                    "total": producto_total
+                }
+                lst_productos.append(producto_data)
+
+            # Crear un diccionario para la factura y agregar la lista de productos
+            factura_data = {
+                "numero": numero,
+                "fecha": fecha,
+                "cliente_nit": cliente_nit,
+                "cliente_nombre": cliente_nombre,
+                "cliente_direccion": cliente_direccion,
+                "total_factura": total_factura,
+                # Agregar la lista de productos a los datos de la factura
+                "lst_productos": lst_productos
+            }
+            lst_facturas.append(factura_data)
+
+    except FileNotFoundError:
+        root_facturas = ET.Element('Facturas')
+        tree_facturas = ET.ElementTree(root_facturas)
+
+    # Renderizar la plantilla con los datos organizados
+    context = {'lst_facturas': lst_facturas}
+    return render(request, "facturas/menu_facturas_editar.html", context)
+
+
+def editar_factura(request, correlativo):
+
+    factura = obtener_factura_por_correlativo(correlativo)
+    clientes = obtener_lista_clientes()
+
+    if factura:
+        # Renderizar el formulario de edición de la factura con los datos existentes
+        context = {'factura': factura, 'clientes': clientes}
+        return render(request, 'facturas/editar_factura.html', context)
+    else:
+        # Manejar el caso en el que la factura no se encuentra
+        return render(request, 'facturas/menu_facturas_crear.html')
+
+
+def guardar_cambios_factura(request, correlativo):
+    if request.method == 'POST':
+        total_factura = 0
+        # Obtener los datos del formulario y guardar los cambios en el archivo XML
+        nit_cliente = request.POST.get('nit_cliente')
+        fecha = request.POST.get('fecha')
+        # total_factura = request.POST.get('totalfactura')
+
+        archivo_xml_path_clientes = os.path.join(
+            settings.BASE_DIR, 'app', 'PuntoDeVenta', 'datos', 'clientes.xml')
+        archivo_xml_path_facturas = os.path.join(
+            settings.BASE_DIR, 'app', 'Factura', 'datos', 'facturas.xml')
+
+        tree_clientes = ET.parse(archivo_xml_path_clientes)
+        root_clientes = tree_clientes.getroot()
+
+        # Buscar el elemento cliente con el NIT especificado y obtener nombre y dirección
+        cliente_element = root_clientes.find(
+            f'.//cliente[@nit="{nit_cliente}"]')
+        if cliente_element is not None:
+            nombre_cliente = cliente_element.find('nombre').text
+            direccion_cliente = cliente_element.find('direccion').text
+        else:
+            # Manejar el caso en que el cliente no se encuentra
+            nombre_cliente = "Cliente no encontrado"
+            direccion_cliente = "Dirección no encontrada"
+
+        # Actualizar la factura
+        tree_facturas = ET.parse(archivo_xml_path_facturas)
+        root_facturas = tree_facturas.getroot()
+
+        # Buscar y actualizar el elemento con el correlativo especificado
+        for factura_element in root_facturas.findall('.//factura'):
+            if factura_element.get('correlativo') == correlativo:
+                # Actualizar datos del cliente
+                cliente_element_factura = factura_element.find('.//cliente')
+                cliente_element_factura.set('nit', nit_cliente)
+                cliente_element_factura.find('nombre').text = nombre_cliente
+                cliente_element_factura.find(
+                    'direccion').text = direccion_cliente
+
+                # Actualizar fecha
+                factura_element.find('fecha').text = fecha
+
+                # Actualizar productos
+                for producto_element in factura_element.findall('.//producto'):
+                    producto_id = producto_element.get('id')
+                    nueva_cantidad = request.POST.get(
+                        f'nueva_cantidad_{producto_id}')
+                    producto_element.find('cantidad').text = nueva_cantidad
+
+                    # Recalcular el total como la multiplicación de la cantidad por el precio unitario
+                    preciounitario = float(
+                        producto_element.find('preciounitario').text)
+                    total = float(nueva_cantidad) * preciounitario
+                    producto_element.find('total').text = str(total)
+                    total_factura += total
+
+                # Recalcular el total de la factura sumando los totales de todos los productos
+                factura_element.find('totalfactura').text = str(total_factura)
+
+        # Guardar los cambios en el archivo XML
+        try:
+            tree_facturas.write(archivo_xml_path_facturas)
+        except Exception as e:
+            print(f"Error al escribir en el archivo XML: {e}")
+
+    # Redirigir de nuevo a la página de listado de facturas
+    return redirect('listar_facturas')
+
+
+def obtener_lista_clientes():
+    archivo_xml_path = os.path.join(
+        settings.BASE_DIR, 'app', 'PuntoDeVenta', 'datos', 'clientes.xml')
+
+    lista_clientes = []
+
+    try:
+        tree = ET.parse(archivo_xml_path)
+        root = tree.getroot()
+
+        for cliente_element in root.findall('.//cliente'):
+            nit = cliente_element.get('nit')
+            nombre = cliente_element.find('nombre').text
+            direccion = cliente_element.find('direccion').text
+
+            cliente_info = {'nit': nit,
+                            'nombre': nombre, 'direccion': direccion}
+            lista_clientes.append(cliente_info)
+
+    except FileNotFoundError:
+        # Si el archivo no existe, puedes manejarlo de alguna manera, como devolver una lista vacía.
+        pass
+
+    return lista_clientes
+
+
+def obtener_factura_por_correlativo(correlativo):
+    archivo_xml_path = os.path.join(
+        settings.BASE_DIR, 'app', 'Factura', 'datos', 'facturas.xml')
+
+    try:
+        tree = ET.parse(archivo_xml_path)
+        root = tree.getroot()
+    except FileNotFoundError:
+        # Si el archivo no existe, retorna None
+        return None
+
+    # Buscar el elemento con el correlativo especificado
+    factura_element = root.find(f'.//factura[@correlativo="{correlativo}"]')
+
+    if factura_element is not None:
+        # Obtener los datos de la factura
+        nit_cliente = factura_element.find('.//cliente').get('nit')
+        nombre_cliente = factura_element.find('.//cliente/nombre').text
+        direccion_cliente = factura_element.find('.//cliente/direccion').text
+        fecha = factura_element.find('fecha').text
+        productos = []
+
+        for producto_element in factura_element.findall('.//producto'):
+            id_producto = producto_element.get('id')
+            nombre_producto = producto_element.find('nombre').text
+            descripcion_producto = producto_element.find('descripcion').text
+            cantidad_producto = producto_element.find('cantidad').text
+            stock_producto = producto_element.find('stock').text
+            precio_unitario_producto = producto_element.find(
+                'preciounitario').text
+            total_producto = producto_element.find('total').text
+
+            producto = {
+                'id': id_producto,
+                'nombre': nombre_producto,
+                'descripcion': descripcion_producto,
+                'cantidad': cantidad_producto,
+                'stock': stock_producto,
+                'precio_unitario': precio_unitario_producto,
+                'total': total_producto
+            }
+
+            productos.append(producto)
+
+        total_factura = factura_element.find('totalfactura').text
+
+        # Crear y retornar un diccionario con los datos de la factura
+        factura = {
+            'correlativo': correlativo,
+            'nit_cliente': nit_cliente,
+            'nombre_cliente': nombre_cliente,
+            'direccion_cliente': direccion_cliente,
+            'fecha': fecha,
+            'productos': productos,
+            'total_factura': total_factura
+        }
+
+        return factura
+    else:
+        # Si no se encuentra la factura, retorna None
+        return None
--- djangoProject/djangoProject/urls.py
+++ djangoProject/djangoProject/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from app.PuntoDeVenta.views import incio, clientes, clientes_crear, clientes_eliminar, clientes_listar, clientes_editar, editar_cliente, guardar_cambios_cliente
 from app.Producto.views import productos, productos_crear, productos_eliminar, productos_listar, productos_editar, editar_producto, guardar_cambios_producto
-from app.Factura.views import facturas, facturas_crear, procesar_pedido, eliminar_factura, listar_facturas
+from app.Factura.views import facturas, facturas_crear, procesar_pedido, eliminar_factura, listar_facturas, facturas_editar, editar_factura, guardar_cambios_factura
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -45,5 +45,10 @@ urlpatterns = [
     path('facturas/crear', facturas_crear, name='facturas_crear'),
     path('procesar_pedido/', procesar_pedido, name='procesar_pedido'),
     path('facturas/eliminar/', eliminar_factura, name='eliminar_factura'),
-    path('facturas/listar/', listar_facturas, name='listar_facturas')
+    path('facturas/listar/', listar_facturas, name='listar_facturas'),
+    path('facturas/editar/', facturas_editar, name='editar_facturas'),
+    path('facturas/editar_producto/<str:correlativo>/',
+         editar_factura, name='editar_factura'),
+    path('guardar_cambios_factura/<str:correlativo>/',
+         guardar_cambios_factura, name='guardar_cambios_factura'),
 ]
